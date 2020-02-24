2c2610c1484c2bd48d23ca68aa625fc5d6212e98
ttdp <tiantong.dev@gmail.com>
2020-02-24 09:23:03 +0800

    Add files

--- Rainbow/ContentView.swift
+++ Rainbow/ContentView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        NavigationView {
+            NavigationLink(destination: SecondaryView()) {
+                Text("Hello, World!")
+            }
+            .navigationBarTitle("Hello")
+        }
     }
 }
 
---
+++ Rainbow/SecondaryView.swift
@@ -0,0 +1,17 @@
+//
+//  SecondaryView.swift
+//  Rainbow
+//
+//  Created by Tian Tong on 2020/2/18.
+//  Copyright © 2020 TTDP. All rights reserved.
+//
+
+import SwiftUI
+
+struct SecondaryView: View {
+    
+    var body: some View {
+        Text("Secondary")
+    }
+    
+}
